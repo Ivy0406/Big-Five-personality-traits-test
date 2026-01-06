d0a54c74587929385a7f2c0fe39e27e76338838f
IvyYang <ivy990030@icloud.com>
2026-01-06 16:08:51 +0800

    feat: import traits data to home page

--- src/components/ui/ShortButton.jsx
+++ src/components/ui/ShortButton.jsx
@@ -3,7 +3,7 @@ function ShortButton() {
     <>
       <button className="w-full max-w-82.5 bg-primary hover:bg-primary-focus active:bg-primary-focus font-sans font-bold text-neutral-main text-[32px] leading-normal py-6 px-18 cursor-pointer flex justify-center items-center gap-2 ">
         <p>開始測驗</p>
-        <span class="material-icons text-[48px]!">arrow_forward</span>
+        <span className="material-icons text-[48px]!">arrow_forward</span>
       </button>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,50 @@
+import { useState, useEffect } from "react";
 import TraitHero from "../components/result/TraitHero";
 import ShortButton from "../components/ui/ShortButton";
+import getTraitsData from "../data/traits-data";
 
 function Home() {
+  const [traitsData, setTraitsData] = useState(null);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const res = await getTraitsData();
+        setTraitsData(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    loadData();
+  }, []);
+
+  if (!traitsData) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center bg-neutral-white">
+        <p className="text-xl text-text-sub">
+          資料載入中...
+          <br />
+          Loading...
+        </p>
+      </div>
+    );
+  }
+
+  const {name, description} = traitsData;
+
+  const cleanZhName = name.zh.split("(")[0].trim();
+  const zhNameLine = {
+    firstLine: cleanZhName.slice(0,6),
+    secondLine: cleanZhName.slice(6)
+    };
+
+  const cleanEnName = name.en.split("(")[0].trim();
+  const sliceIndex = cleanEnName.indexOf("traits");
+  const enNameLine = {
+    firstLine: cleanEnName.slice(0,sliceIndex).trim(),
+    secondLine: cleanEnName.slice(sliceIndex).trim()
+  }
+
   return (
     <>
       <TraitHero traitType="home" />
@@ -9,16 +52,21 @@ function Home() {
         <div className="w-full max-w-352.5 mx-auto flex flex-col items-center gap-4">
           <div className="w-full flex flex-col lg:flex-row justify-center lg:justify-start items-center lg:items-end">
             <h1 className="text-center lg:text-left text-5xl lg:text-[64px] leading-normal font-light text-text-main">
-              五大性格特質
+              {zhNameLine.firstLine}
               <br className="hidden lg:block" />
-              心理測驗
+              {zhNameLine.secondLine}
             </h1>
-            <div className="flex justify-center items-center text-xl lg:text-2xl font-light text-text-main w-fit h-auto lg:h-23.5 lg:-ml-[7.5%]"><p className="w-full">Big Five personality<br className="hidden lg:block"/>traits test</p></div>
+            <div className="flex justify-center items-center text-xl lg:text-2xl font-light text-text-main w-fit h-auto lg:h-23.5 lg:-ml-[7.5%]">
+              <p className="w-full">
+                {enNameLine.firstLine}
+                <br className="hidden lg:block" />
+                {enNameLine.secondLine}
+              </p>
+            </div>
           </div>
           <div className="w-full flex flex-col items-center lg:flex-row lg:justify-end gap-7.5">
             <p className="text-[16px] leading-normal text-text-sub w-full max-w-150 lg:w-112.5">
-              五大性格特質（Big Five personality
-              traits）在心理學中常被作為人格的評估，有時也會用來預測學業成就、以及作為適合職業的參考。此理論認為人的主要性格由五項人格特質組成：經驗開放性、盡責性、外向性、親和性、情緒不穩定性。
+              {description}
             </p>
             <ShortButton className="lg:min-w-82.5" />
           </div>
