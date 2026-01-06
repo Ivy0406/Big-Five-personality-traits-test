26bdfda9ac525eb112eee09643f1128bd78cf9b0
IvyYang <ivy990030@icloud.com>
2026-01-06 10:34:33 +0800

    feat: add icon button

---
+++ src/components/ui/IconButton.jsx
@@ -0,0 +1,11 @@
+function IconButton() {
+  return (
+    <>
+      <button className="bg-primary hover:bg-primary-focus active:bg-primary-focus font-sans font-bold text-neutral-main p-6 cursor-pointer flex justify-center items-center w-24 aspect-square">
+        <span class="material-icons text-[48px]!">arrow_back</span>
+      </button>
+    </>
+  );
+}
+
+export default IconButton;
