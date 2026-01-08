355755b80c5e996955116c6afc3254180f6958af
IvyYang <ivy990030@icloud.com>
2026-01-09 00:50:01 +0800

    feat: create question component

---
+++ src/components/quiz/Question.jsx
@@ -0,0 +1,24 @@
+
+const Question = ({ question, categoryInfo }) => {
+  return (
+    <div className="px-24 flex flex-col justify-end gap-50 lg:gap-112.25">
+      <div className="flex flex-col gap-4">
+        <p className="text-[120px] italic font-en text-text-main">Q</p>
+        <p className="text-5xl font-light text-text-main pl-12 h-28.5">
+          {question}
+        </p>
+      </div>
+
+      <div>
+        <p className="text-text-minor text-[16px] leading-normal">
+          {categoryInfo.zh}
+        </p>
+        <p className="text-text-minor text-[12px] leading-normal">
+          {categoryInfo.en}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
