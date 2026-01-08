3f2d8c9ff767d8cc6b708209895f672f69ff6655
IvyYang <ivy990030@icloud.com>
2026-01-09 00:51:44 +0800

    fix: resolve parameter typo

--- src/components/quiz/Question.jsx
+++ src/components/quiz/Question.jsx
@@ -1,5 +1,5 @@
 
-const Question = ({ question, categoryInfo }) => {
+const Question = ({ question, traitName }) => {
   return (
     <div className="px-24 flex flex-col justify-end gap-50 lg:gap-112.25">
       <div className="flex flex-col gap-4">
@@ -11,10 +11,10 @@ const Question = ({ question, categoryInfo }) => {
 
       <div>
         <p className="text-text-minor text-[16px] leading-normal">
-          {categoryInfo.zh}
+          {traitName.zh}
         </p>
         <p className="text-text-minor text-[12px] leading-normal">
-          {categoryInfo.en}
+          {traitName.en}
         </p>
       </div>
     </div>
