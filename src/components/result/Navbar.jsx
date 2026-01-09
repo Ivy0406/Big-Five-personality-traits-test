a3ee59788c3e2fe2cc868971ecfa58d11605e4fc
IvyYang <ivy990030@icloud.com>
2026-01-09 15:27:04 +0800

    update: adjust navbar key

--- src/components/result/Navbar.jsx
+++ src/components/result/Navbar.jsx
@@ -2,7 +2,9 @@ const Navbar = ({ options }) => {
   return (
     <ul className="flex justify-between gap-12">
       {options.map((option) => (
-        <li className="w-fit h-8 text-neutral-main font-light cursor-pointer hover:font-bold active:font-bold hover:border-b-4 hover:border-primary active:border-b-4 active:border-primary">
+        <li 
+        key={option}
+        className="w-fit h-8 text-neutral-main font-light cursor-pointer hover:font-bold active:font-bold hover:border-b-4 hover:border-primary active:border-b-4 active:border-primary">
           <a className="w-full h-full">{option}</a>
         </li>
       ))}
