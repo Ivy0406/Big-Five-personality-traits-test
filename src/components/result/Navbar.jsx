09f929442716fb7be5a1d0c2bf1cb4f8dac474f7
IvyYang <ivy990030@icloud.com>
2026-01-09 14:43:04 +0800

    feat: create navbar component

---
+++ src/components/result/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = ({ options }) => {
+  return (
+    <ul className="flex justify-between gap-12">
+      {options.map((option) => (
+        <li className="w-fit h-8 text-neutral-main font-light cursor-pointer hover:font-bold active:font-bold hover:border-b-4 hover:border-primary active:border-b-4 active:border-primary">
+          <a className="w-full h-full">{option}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Navbar;
