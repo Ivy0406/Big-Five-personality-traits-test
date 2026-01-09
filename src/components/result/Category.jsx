aa692637744ffc643f475ac210561624926319e0
IvyYang <ivy990030@icloud.com>
2026-01-09 15:26:10 +0800

    feat: create category component

---
+++ src/components/result/Category.jsx
@@ -0,0 +1,11 @@
+const Category = ({title, description}) => {
+    return ( <div className="flex flex-col gap-12 xl:flex-row xl:gap-19">
+        <div>
+            <p className="text-neutral-main font-light text-5xl leading-normal">{title.zh}</p>
+            <p className="text-neutral-main text-2xl font-light leading-normal">{title.en}</p>
+        </div>
+        <p className="text-neutral-sub w-full max-w-112.5 ">{description}</p>
+        </div> );
+}
+ 
+export default Category;
--- src/components/result/TraitHero.jsx
+++ src/components/result/TraitHero.jsx
@@ -1,10 +1,12 @@
-const TraitHero = ({traitType}) => {
+const TraitHero = ({traitType , children="", className}) => {
     const bgImg = new URL(`../../assets/images/banner-${traitType}.png`,import.meta.url).href;
     return ( 
-        <>
-        <div className="w-full h-135 bg-cover bg-center" 
-      style={{ backgroundImage: `url(${bgImg})` }}></div>
-        </>
+      
+        <div className={`w-full h-135 bg-cover bg-center ${className}`} 
+      style={{ backgroundImage: `url(${bgImg})` }}>
+        {children}
+      </div>
+      
      );
 }
  
