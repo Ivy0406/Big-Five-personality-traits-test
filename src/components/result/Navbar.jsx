dd1b91408a2c9e4edf72535e1e1293ff5630e420
IvyYang <ivy990030@icloud.com>
2026-01-11 17:52:59 +0800

    update: adjust navbar props

--- src/components/result/Navbar.jsx
+++ src/components/result/Navbar.jsx
@@ -1,13 +1,27 @@
-const Navbar = ({ options }) => {
+const Navbar = ({ options ,activeKey, onSelect}) => {
   return (
     <ul className="flex justify-between gap-12">
-      {options.map((option) => (
-        <li 
-        key={option}
-        className="w-fit h-8 text-neutral-main font-light cursor-pointer hover:font-bold active:font-bold hover:border-b-4 hover:border-primary active:border-b-4 active:border-primary">
-          <a className="w-full h-full">{option}</a>
-        </li>
-      ))}
+      {options.map((option) => {
+        const isActive = activeKey === option.key;
+
+        return (
+          <li
+            key={option.key}
+            onClick={() => onSelect(option.key)}
+            className={`
+              w-fit h-8 text-neutral-main cursor-pointer 
+              hover:font-bold hover:border-b-4 hover:border-primary 
+              ${
+                isActive
+                  ? "font-bold border-b-4 border-primary" 
+                  : "font-light"                          
+              }
+            `}
+          >
+            <a className="w-full h-full block">{option.label}</a>
+          </li>
+        );
+      })}
     </ul>
   );
 };
