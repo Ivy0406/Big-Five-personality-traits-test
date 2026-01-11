3b610bbe9d5d42058aeb0240da42c708f45b6624
IvyYang <ivy990030@icloud.com>
2026-01-12 00:03:42 +0800

    fix: resolve capitalization for categoty title

--- src/components/result/Category.jsx
+++ src/components/result/Category.jsx
@@ -2,7 +2,7 @@ const Category = ({title, description}) => {
     return ( <div className="flex flex-col gap-12 px-8 xl:flex-row xl:gap-19">
         <div>
             <p className="text-neutral-main font-light text-5xl leading-normal">{title.zh}</p>
-            <p className="text-neutral-main text-2xl font-light leading-normal">{title.en}</p>
+            <p className="text-neutral-main text-2xl font-light leading-normal capitalize">{title.en}</p>
         </div>
         <p className="text-neutral-sub w-full max-w-112.5 ">{description}</p>
         </div> );
